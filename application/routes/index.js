741cdbbd2685fb554b5059365e5776935572880f
Kaiki Deishuu <acarlson9000@gmail.com>
2021-05-02 00:52:22 -0700

    Milestone 3

--- application/routes/index.js
+++ application/routes/index.js
@@ -3,27 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {name: "Aaron"});
+  res.render('index', {name: "Aaron"}, {title: "Aaron's App"});
 });
 
 router.get('/home', (req, res, next) => {
-  res.render("home");
+  res.render("home", {title: "Home"});
 });
 
 router.get('/login', (req, res, next) => {
-  res.render("login");
+  res.render("login", {title: "Log In"});
 });
 
 router.get('/registration', (req, res, next) => {
-  res.render("registration");
+  res.render("registration", {title: "Register"});
 });
 
 router.get('/postimage', (req, res, next) => {
-  res.render("postimage");
+  res.render("postimage", {title: "Create a Post"});
 });
 
 router.get('/imagepost', (req, res, next) => {
-  res.render("imagepost");
+  res.render("imagepost", {title: "Post"});
 });
 
 module.exports = router;
