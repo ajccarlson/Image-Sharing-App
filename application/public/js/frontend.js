38fc9b16c2bed5b7bb0dfd8c50cb5a977c85fab0
Kaiki Deishuu <acarlson9000@gmail.com>
2021-05-10 22:13:59 -0700

    Milestone 4

--- application/public/js/frontend.js
+++ application/public/js/frontend.js
@@ -1,18 +1,71 @@
-function setFlashMessageFadeOut() {
+function setFlashMessageFadeOut(flashMessageDElement) {
     setTimeout(() => {
         let currentOpacity = 1.0;
         let timer = setInterval(() => {
             if (currentOpacity < 0.05) {
                 clearInterval(timer);
-                flashElement.remove();
+                flashMessageDElement.remove();
             }
             currentOpacity = currentOpacity - 0.05;
-            flashElement.style.opacity = currentOpacity;
+            flashMessageDElement.style.opacity = currentOpacity;
         }, 50)
     }, 4000);
 }
 
+function addFlashFromFrontEnd(message) {
+    let flashMessageDiv = document.createElement('div');
+    let innerFlashDiv = document.createElement('div');
+    let innerTextNode = document.createTextNode(message);
+    innerFlashDiv.appendChild(innerTextNode);
+    flashMessageDiv.appendChild(innerFlashDiv);
+    flashMessageDiv.setAttribute('id', 'flash-message');
+    innerFlashDiv.setAttribute('class', 'alert alert-info');
+    document.getElementsByTagName('body')[0].appendChild(flashMessageDiv);
+    setFlashMessageFadeOut(flashMessageDiv);
+}
+
+function createCard(postData) {
+    return `<div id="post-${postData.id}" class="card">
+    <img class="card-image" src="${postData.thumbnail}" alt="Missing Image">
+    <div class="card-body">
+        <p class="card-title">${postData.title}</p>
+        <p class="card-text">${postData.description}</p>
+        <a href="/post/${postData.id}" class="anchor-buttons">Post Details</a>
+    </div>
+</div>`;
+}
+
+function executeSearch() {
+    let searchTerm = document.getElementById('search-text').value;
+    if (!searchTerm) {
+        location.replace('/');
+        return;
+    }
+    let mainConent = document.getElementById('home-content');
+    let searchUrl = `/posts/search?search=${searchTerm}`;
+    fetch(searchUrl)
+    .then((data) => {
+        return data.json();
+    })
+    .then((data_json) => {
+        let newMainContentHTML ='';
+        data_json.results.forEach(row => {
+            newMainContentHTML += createCard(row);
+        });
+        mainConent.innerHTML = newMainContentHTML;
+        if (data_json.message) {
+            addFlashFromFrontEnd(data_json.message);
+        }
+    })
+    .catch((err) => console.log(err));
+}
+
 let flashElement = document.getElementById('flash-message');
 if (flashElement) {
-    setFlashMessageFadeOut();
+    setFlashMessageFadeOut(flashMessageDElement);
+}
+
+let searchButton = document.getElementById('search-button');
+if (searchButton) {
+    searchButton.onclick = executeSearch;
 }
